b16df7c2444a84bf016633241e413d40065377c2
Qian Huanping <qian.huanping@outlook.com>
2021-04-07 21:30:42 +0800

    Initial commit

---
+++ src/test/java/com/huawei/hcs/practice/activemq/HcsPracticeActivemqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huawei.hcs.practice.activemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcsPracticeActivemqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
